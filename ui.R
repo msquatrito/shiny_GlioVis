083daa4de9a6cbeaad41b06ca511e4c95f205fcc
msquatrito <m_squatrito@yahoo.com>
2014-10-09 17:49:36 +0200

    new app structure

---
+++ ui.R
@@ -0,0 +1,16 @@
+library(shiny)
+library(shinysky)
+source("global.R")
+
+shinyUI(  
+  
+  navbarPage(title = "GlioVis", windowTitle = "GlioVis - Visualization Tools for Glioma Datasets", fluid = TRUE, responsive = TRUE,
+             footer = includeHTML("tools/footer.html"),
+             
+             source("tools/homeTab.R", local = TRUE)$value,  
+             source("tools/exploreTab.R", local = TRUE)$value,
+             source("tools/toolsTab.R", local = TRUE)$value,
+             source("tools/aboutTab.R", local = TRUE)$value
+             
+  )
+)
