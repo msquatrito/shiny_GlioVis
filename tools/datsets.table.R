22f796976d120fa5faf85d667d37be05507c2ba1
msquatrito <m_squatrito@yahoo.com>
2014-12-16 12:46:42 +0100

    formatted table with links

--- tools/datsets.table.R
+++ tools/datsets.table.R
@@ -1,23 +1,28 @@
 HTML('<style type="text/css">
+
 .tg  {border-collapse:collapse;border-spacing:0;border-width:1px;border-style:solid;box-shadow: 10px 10px 5px #888888;}
 .tg td{font-family:Arial, sans-serif;font-size:14px;padding: 0px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
      .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding: 0px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
      .tg .tg-lvt2{font-size:14px;background-color:#68cbd0;color:#333333;text-align:center;padding: 0px 5px}
      .tg .tg-eccc{font-weight:bold;font-style:italic;font-size:16px;padding: 0px 5px}
      .tg .tg-s6z2{text-align:center;padding: 0px 5px}
-     .tg .tg-0a3w{background-color:#68cbd0;padding: 0px 5px}
+     .tg .tg-s6z2-bd{text-align:center;padding: 0px 5px;border-right: 1px dashed gray;}
+     .tg .tg-0a3w{background-color:#68cbd0;padding: 0px 5px;border-right: 1px dashed gray;}
      .tg .tg-cxkv{background-color:#ffffff;padding: 0px 5px}
      .tg .tg-eoji{font-size:14px;color:#333333;text-align:center;padding: 0px 5px}
      .tg .tg-9fhq{background-color:#68cbd0;text-align:center;padding: 0px 5px}
-     .tg .tg-hgcj{font-weight:bold;text-align:center;padding: 0px 5px}
+     .tg .tg-9fhq-bd{background-color:#68cbd0;text-align:center;padding: 0px 5px;border-right: 1px dashed gray;}
+     .tg .tg-hgcj{font-weight:bold;text-align:center;padding: 0px 5px;}
+     .tg .tg-hgcj-bd{font-weight:bold;text-align:center;padding: 0px 5px;border-right: 1px dashed gray;}
+    .tg .tg-bd{border-right: 1px dashed gray;}
 tfoot {color:gray; font-size:6px; text-align: right;}
      </style>
      <table class="tg">
      <tr>
-     <th class="tg-031e"><br></th>
+     <th class="tg-bd"><br></th>
      <th class="tg-031e"></th>
      <th class="tg-eccc">Samples</th>
-     <th class="tg-031e"></th>
+     <th class="tg-s6z2-bd"></th>
      <th class="tg-031e"></th>
      <th class="tg-031e"></th>
      <th class="tg-031e"><br></th>
@@ -28,10 +33,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <th class="tg-031e"></th>
      </tr>
      <tr>
-     <td class="tg-031e"></td>
+     <td class="tg-hgcj-bd"></td>
      <td class="tg-hgcj">Non-tumor</td>
      <td class="tg-hgcj">Low-grade</td>
-     <td class="tg-hgcj">GBM</td>
+     <td class="tg-hgcj-bd">GBM</td>
      <td class="tg-hgcj"></td>
      <td class="tg-hgcj">Histology</td>
      <td class="tg-hgcj">Grade</td>
@@ -42,10 +47,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-hgcj">Survival</td>
      </tr>
      <tr>
-     <td class="tg-0a3w">TCGA GBM</td>
+     <td class="tg-0a3w"><a href="http://www.ncbi.nlm.nih.gov/pubmed/18772890" style="color: rgb(0,0,0)">TCGA GBM</a></td>
      <td class="tg-9fhq">10</td>
      <td class="tg-9fhq">0</td>
-     <td class="tg-9fhq">528</td>
+     <td class="tg-9fhq-bd">528</td>
      <td class="tg-9fhq"></td>
      <td class="tg-lvt2">✔</td>
      <td class="tg-lvt2">-</td>
@@ -56,10 +61,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-lvt2">✔</td>
      </tr>
      <tr>
-     <td class="tg-031e">TCGA LGG</td>
+     <td class="tg-bd"><a href="https://tcga-data.nci.nih.gov/tcga/tcgaCancerDetails.jsp?diseaseType=LGG&diseaseName=Brain%20Lower%20Grade%20Glioma" style="color: rgb(0,0,0)">TCGA LGG</a></td>
      <td class="tg-s6z2">0</td>
-     <td class="tg-s6z2">463</td>
-     <td class="tg-s6z2">0</td>
+     <td class="tg-s6z2">513</td>
+     <td class="tg-s6z2-bd">0</td>
      <td class="tg-s6z2"></td>
      <td class="tg-eoji">✔</td>
      <td class="tg-eoji">✔</td>
@@ -70,10 +75,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-eoji">✔</td>
      </tr>
      <tr>
-     <td class="tg-0a3w">Rembrandt</td>
+     <td class="tg-0a3w"><a href="http://www.ncbi.nlm.nih.gov/pubmed/19208739" style="color: rgb(0,0,0)">Rembrandt</a></td>
      <td class="tg-9fhq">21</td>
      <td class="tg-9fhq">200</td>
-     <td class="tg-9fhq">184</td>
+     <td class="tg-9fhq-bd">184</td>
      <td class="tg-9fhq"></td>
      <td class="tg-lvt2">✔</td>
      <td class="tg-lvt2">✔</td>
@@ -84,10 +89,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-lvt2">✔</td>
      </tr>
      <tr>
-     <td class="tg-031e">Gravendeel</td>
+     <td class="tg-bd"><a href="http://www.ncbi.nlm.nih.gov/pubmed/19920198" style="color: rgb(0,0,0)">Gravendeel</a></td>
      <td class="tg-s6z2">8</td>
      <td class="tg-s6z2">117</td>
-     <td class="tg-s6z2">159</td>
+     <td class="tg-s6z2-bd">159</td>
      <td class="tg-s6z2"></td>
      <td class="tg-eoji">✔</td>
      <td class="tg-eoji">✔</td>
@@ -98,10 +103,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-eoji">✔</td>
      </tr>
      <tr>
-     <td class="tg-0a3w">Phillips</td>
+     <td class="tg-0a3w"><a href="http://www.ncbi.nlm.nih.gov/pubmed/16530701" style="color: rgb(0,0,0)">Phillips</a></td>
      <td class="tg-9fhq">0</td>
      <td class="tg-9fhq">0</td>
-     <td class="tg-9fhq">100</td>
+     <td class="tg-9fhq-bd">100</td>
      <td class="tg-9fhq"></td>
      <td class="tg-lvt2">-</td>
      <td class="tg-lvt2">✔</td>
@@ -112,10 +117,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-lvt2">✔</td>
      </tr>
      <tr>
-     <td class="tg-031e">Murat</td>
+     <td class="tg-bd"><a href="http://www.ncbi.nlm.nih.gov/pubmed/18565887" style="color: rgb(0,0,0)">Murat</a></td>
      <td class="tg-s6z2">4</td>
      <td class="tg-s6z2">0</td>
-     <td class="tg-s6z2">80</td>
+     <td class="tg-s6z2-bd">80</td>
      <td class="tg-s6z2"></td>
      <td class="tg-eoji">✔</td>
      <td class="tg-eoji">-</td>
@@ -126,10 +131,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-eoji">✔</td>
      </tr>
      <tr>
-     <td class="tg-0a3w">Freije</td>
+     <td class="tg-0a3w"><a href="http://www.ncbi.nlm.nih.gov/pubmed/15374961" style="color: rgb(0,0,0)">Freije</a></td>
      <td class="tg-9fhq">0</td>
      <td class="tg-9fhq">26</td>
-     <td class="tg-9fhq">59</td>
+     <td class="tg-9fhq-bd">59</td>
      <td class="tg-9fhq"></td>
      <td class="tg-lvt2">✔</td>
      <td class="tg-lvt2">✔</td>
@@ -140,10 +145,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-lvt2">✔</td>
      </tr>
      <tr>
-     <td class="tg-031e">Reifenberger</td>
+     <td class="tg-bd"><a href="http://www.ncbi.nlm.nih.gov/pubmed/24615357" style="color: rgb(0,0,0)">Reifenberger</a></td>
      <td class="tg-s6z2">0</td>
      <td class="tg-s6z2">0</td>
-     <td class="tg-s6z2">70</td>
+     <td class="tg-s6z2-bd">70</td>
      <td class="tg-s6z2"></td>
      <td class="tg-eoji">-</td>
      <td class="tg-eoji">-</td>
@@ -154,10 +159,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-eoji">-</td>
      </tr>
      <tr>
-     <td class="tg-0a3w">Bao</td>
+     <td class="tg-0a3w"><a href="http://www.ncbi.nlm.nih.gov/pubmed/25135958" style="color: rgb(0,0,0)">Bao</a></td>
      <td class="tg-9fhq">0</td>
      <td class="tg-9fhq">174</td>
-     <td class="tg-9fhq">100</td>
+     <td class="tg-9fhq-bd">100</td>
      <td class="tg-9fhq"></td>
      <td class="tg-lvt2">✔</td>
      <td class="tg-lvt2">-</td>
@@ -168,10 +173,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-lvt2">-</td>
      </tr>
      <tr>
-     <td class="tg-031e">Gill</td>
+     <td class="tg-bd"><a href="http://www.ncbi.nlm.nih.gov/pubmed/25114226" style="color: rgb(0,0,0)">Gill</a></td>
      <td class="tg-s6z2">17</td>
      <td class="tg-s6z2">0</td>
-     <td class="tg-s6z2">75*</td>
+     <td class="tg-s6z2-bd">75*</td>
      <td class="tg-s6z2"></td>
      <td class="tg-eoji">✔</td>
      <td class="tg-eoji">-</td>
@@ -182,10 +187,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-eoji">-</td>
      </tr>
      <tr>
-     <td class="tg-0a3w">Gorovets</td>
+     <td class="tg-0a3w"><a href="http://www.ncbi.nlm.nih.gov/pubmed/22415316" style="color: rgb(0,0,0)">Gorovets</a></td>
      <td class="tg-9fhq">1</td>
      <td class="tg-9fhq">80</td>
-     <td class="tg-9fhq">0</td>
+     <td class="tg-9fhq-bd">0</td>
      <td class="tg-9fhq"></td>
      <td class="tg-lvt2">✔</td>
      <td class="tg-lvt2">✔</td>
@@ -195,10 +200,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-lvt2">✔</td>
      <td class="tg-lvt2">✔</td>
      </tr>
-     <td class="tg-031e">Nutt</td>
+     <td class="tg-bd"><a href="http://www.ncbi.nlm.nih.gov/pubmed/12670911" style="color: rgb(0,0,0)">Nutt</a></td>
      <td class="tg-s6z2">0</td>
      <td class="tg-s6z2">22</td>
-     <td class="tg-s6z2">28</td>
+     <td class="tg-s6z2-bd">28</td>
      <td class="tg-s6z2"></td>
      <td class="tg-eoji">✔</td>
      <td class="tg-eoji">-</td>
@@ -209,10 +214,10 @@ tfoot {color:gray; font-size:6px; text-align: right;}
      <td class="tg-eoji">✔</td>
      </tr>
      <tr>
-     <td class="tg-0a3w">Ducray</td>
+     <td class="tg-0a3w"><a href="http://www.ncbi.nlm.nih.gov/pubmed/20822523" style="color: rgb(0,0,0)">Ducray</a></td>
      <td class="tg-9fhq">0</td>
      <td class="tg-9fhq">0</td>
-     <td class="tg-9fhq">52</td>
+     <td class="tg-9fhq-bd">52</td>
      <td class="tg-9fhq"></td>
      <td class="tg-lvt2">-</td>
      <td class="tg-lvt2">-</td>
