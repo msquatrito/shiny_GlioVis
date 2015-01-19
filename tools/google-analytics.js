5256654afc787207886998bdfef97eae7a8f5252
squatrim <squatrim@gmail.com>
2015-01-19 10:48:15 +0100

    updated

--- tools/google-analytics.js
+++ tools/google-analytics.js
@@ -4,5 +4,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-54611114-1', 'auto');
+  ga('create', 'UA-54611114-2', 'auto');
   ga('send', 'pageview');
