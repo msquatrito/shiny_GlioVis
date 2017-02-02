fd332593df06a5d009a70036580d32e6c89ae908
squatrim <squatrim@gmail.com>
2017-02-02 11:30:43 +0100

    Reformatted about Tab

--- tabs/aboutTab.R
+++ tabs/aboutTab.R
@@ -1,6 +1,11 @@
 # UI-elements for About tab
 tabPanel(title = "About", icon = icon("info-circle"),
-         div(class="about",
-             includeMarkdown("tools/about.Rmd")
-         )
+         
+         panel_div(class_type = "primary",panel_title = "Data Processing",content = includeMarkdown("tools/Processing.Rmd")),
+         panel_div(class_type = "primary",panel_title = "Classification",content = includeMarkdown("tools/Classification.Rmd")),
+         panel_div(class_type = "primary",panel_title = "Funding",
+                  content = HTML ('<a href ="http://www.seveballesteros.com"><img src="seve.jpg" height = "135"  width = "405"></a>')),
+         panel_div(class_type = "primary",panel_title = "App info",content = includeMarkdown("tools/App_info.Rmd")),
+         panel_div(class_type = "primary",panel_title = "License",content = includeMarkdown("tools/License.Rmd"))
+
 )
