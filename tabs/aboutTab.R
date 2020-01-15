a7809838f90fa12a7b1b327221e301632b2b08d9
squatrim <squatrim@gmail.com>
2020-01-15 16:43:36 +0100

    Added session info panel

--- tabs/aboutTab.R
+++ tabs/aboutTab.R
@@ -6,6 +6,7 @@ tabPanel(title = "About", icon = icon("info-circle"),
          panel_div(class_type = "primary",panel_title = "Funding",
                   content = HTML ('<a href ="http://www.seveballesteros.com"><img src="seve.jpg" height = "135"  width = "405"></a>')),
          panel_div(class_type = "primary",panel_title = "App info",content = includeMarkdown("tools/App_info.Rmd")),
+         panel_div(class_type = "primary",panel_title = "Session info",content = htmlOutput("sessionInfo")),
          panel_div(class_type = "primary",panel_title = "License",content = includeMarkdown("tools/License.Rmd"))
 
 )
